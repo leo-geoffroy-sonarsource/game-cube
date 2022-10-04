b6b9828f0dde786f1e5d4a01b07a1d386bbd603f
Léo Geoffroy <leo.geoffroy@sonarsource.com>
2022-10-04 14:35:01 +0200

    Add Well to the choices

--- backend/game-qube/src/main/java/org/qubee/rps/RpsActionType.java
+++ backend/game-qube/src/main/java/org/qubee/rps/RpsActionType.java
@@ -9,15 +9,24 @@ public enum RpsActionType implements ActionType {
   @JsonProperty("PAPER")
   PAPER,
   @JsonProperty("ROCK")
-  SCISSORS;
+  SCISSORS,
+
+  @JsonProperty("WELL")
+  WELL;
 
   public boolean winAgainst(ActionType actionType) {
+    if (this.equals(WELL) && (actionType.equals(SCISSORS) || actionType.equals(ROCK))){
+      return true;
+    }
+
     if (this.equals(ROCK) && actionType.equals(SCISSORS)) {
       return true;
     }
-    if (this.equals(PAPER) && actionType.equals(ROCK)) {
+
+    if (this.equals(PAPER) && (actionType.equals(ROCK) || actionType.equals(WELL))) {
       return true;
     }
+
     return this.equals(SCISSORS) && actionType.equals(PAPER);
   }
 }
